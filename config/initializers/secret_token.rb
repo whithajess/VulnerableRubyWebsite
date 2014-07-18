9bb1de0b8b29669706b3192dce59d3f05fa541dc
jesse <jesse@auraredeye.com>
2014-07-18 12:45:07 +1200

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VulnerableRubyWebsite::Application.config.secret_token = 'd87efad8e90bd9d4ba9d5199f8d6e3a557f5897a4cc350d5c598b2e4d2f2c639ff7a2ddc0900890367f354481fb905015dd327e0d0606f949b34e35c5f46bcbf'
